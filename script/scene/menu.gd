7de966c4e729803f00a366595b770458efa5a253
Nah man, i lose. <selaruang@gmail.com>
2025-08-22 23:09:31 +0700

    Reapply "add menu scene"
    This reverts commit 2ff68c1fa7a82a1421456a83f7d1445da6207586.

---
+++ script/scene/menu.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _ready() -> void:
+	$nobura.play("1")
+	$ume.play("1")
+	
+
+
+func _on_play_button_pressed() -> void:
+	print("Play Pressed")
+
+
+func _on_exit_button_pressed() -> void:
+	print("Exit Pressed")
