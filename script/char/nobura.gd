56cdf946778699de9cb612ef2b3d73c25b6f1a38
Nah man, i lose. <selaruang@gmail.com>
2025-08-23 22:19:03 +0700

    add char scene and test scene

---
+++ script/char/nobura.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+# The speed of the character
+@export var speed = 300.0
+
+func _ready() -> void:
+	# Get the viewport size
+	#var window_size = get_viewport_rect()
+	## Set the initial position to the center of the screen
+	#position = window_size.size / 2
+	pass
+	
+func _physics_process(delta: float) -> void:
+	movement()
+
+func movement() -> void:
+	var input_direction = Vector2.ZERO
+	
+	# Get input from keyboard
+	if Input.is_action_pressed("up"):
+		input_direction.y -= 1
+	if Input.is_action_pressed("down"):
+		input_direction.y += 1
+	if Input.is_action_pressed("left"):
+		input_direction.x -= 1
+	if Input.is_action_pressed("right"):
+		input_direction.x += 1
+		
+	# Normalize the vector to prevent faster diagonal movement
+	velocity = input_direction.normalized() * speed
+	
+	# Call the built-in Godot function to move the character
+	move_and_slide()
